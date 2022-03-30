006e03fb5990a5b20e20092bb93edde6edf14d30
Serhii Dvorets <dvorets.sergey@gmail.com>
2022-03-30 13:16:00 +0300

    solution

---
+++ src/components/NoResult.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NoResult = () => {
+  return (
+    <div className="no-result">
+      <h2 className="section__title">None for today, sorry!</h2>
+    </div>
+  );
+};
